836e51855ee35cbf7bea33481e59cd4d06c117f5
win <abudimnasr@gmail.com>
2024-05-04 23:18:41 +0300

    working on adding the fragment

--- app/src/main/java/com/example/taskmanagement/pages/AddNoteFragment.java
+++ app/src/main/java/com/example/taskmanagement/pages/AddNoteFragment.java
@@ -1,20 +1,33 @@
 package com.example.taskmanagement;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.taskmanagement.FireeBase.FirebaseServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.installations.Utils;
+
+import java.util.UUID;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,6 +48,10 @@ public class AddNoteFragment extends Fragment {
 
 
     private ArrayAdapter<CharSequence> colorAdapter;
+
+
+    String[] Importance={"Very Important", "Important","Not Important"};
+
 
 
 
@@ -91,5 +108,244 @@ public class AddNoteFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_note, container, false);
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        init();
+    }
+
+    private void init() {
+        // ---->    פרטי הוספת רכב    <----
+        //editText
+        fbs=FirebaseServices.getInstance();
+        utils = Utils.getInstance();
+        titleInput=getView().findViewById(R.id.titleinput);
+        descriptionInput=getView().findViewById(R.id.descriptioninput);
+        saveBtn=getView().findViewById(R.id.savebtn);
+        img = getView().findViewById(R.id.ivCarAddCarFragment);
+
+        //spinner for the color of car
+        spnImp=getView().findViewById(R.id.spnImp);
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(getActivity(), R.layout.item_file,Importance);
+        adapter.setDropDownViewResource(R.layout.item_file);
+        spnImp.setAdapter(adapter);
+
+        spnImp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String value=parent.getItemAtPosition(position).toString();
+                //   Toast.makeText(getActivity(), value, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        //spinner for the year of car
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+
+
+        btnAddCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // adding to firestore  'car' collection
+
+                addToFirestore();
+            }
+        });
+//////////////////////try spinner////////////////////////////////////////////////////////
+
+        colorAdapter= ArrayAdapter.createFromResource(getActivity(),R.array.array_colors,R.layout.spinner_layout);
+        ////////////////////////////////try new spinner ///////////////////////////
+        colorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        colorSpinner.setAdapter(colorAdapter);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+        ((MainActivity)getActivity()).pushFragment(new AddCarFragment());
+    }
+
+    private void addToFirestore() {
+
+        String nameCar, horse_power, owners, phone,
+                car_num, manufacturer, year, car_type, Car_model,
+                test ,kilometre,Engine_capacity,Gear_shifting_model,price ;
+        String color;
+//get data from screen
+
+        nameCar=etnameCar.getText().toString();
+        horse_power=ethorse_power.getText().toString();
+        owners=etOwners.getText().toString();
+        color=colorSpinner.getSelectedItem().toString();
+        phone = etPhone.getText().toString();
+        car_num=etCar_num.getText().toString();
+        manufacturer=etManufacturer.getText().toString();
+        year=yearOfCarSpinner.getSelectedItem().toString();
+        Car_model=etCar_model.getText().toString();
+        test=etTest.getText().toString();
+        kilometre=etkilometre.getText().toString();
+        Engine_capacity=etEngine_capacity.getText().toString();
+        Gear_shifting_model="DSG";
+        price=etPrice.getText().toString();
+        //מספר טלפון לא חייב לבדוק
+//        if(phone==null){
+//            phone="-";
+//            return;
+//        }
+        if (nameCar.trim().isEmpty()                                        ||
+                horse_power.trim().isEmpty()                                ||
+                owners.trim().isEmpty()                                     ||
+                color.trim().isEmpty()                                      ||
+                car_num.trim().isEmpty()                                    ||
+                manufacturer.trim().isEmpty()                               ||
+                year.trim().isEmpty()                                       ||
+                Car_model.trim().isEmpty()                                  ||
+                test.trim().isEmpty()                                       ||
+                kilometre.trim().isEmpty()                                  ||
+                Engine_capacity.trim().isEmpty()                            ||
+                Gear_shifting_model.trim().isEmpty()                        ||
+                price.trim().isEmpty())
+
+        {
+            Toast.makeText(getActivity(), "sorry some data missing incorrect !", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Car car;
+        CarItem car2;
+        if (fbs.getSelectedImageURL() == null)
+        {
+            car= new Car(nameCar, horse_power, owners, phone, color ,
+                    car_num, manufacturer, year, Car_model,
+                    test ,kilometre,Engine_capacity,Gear_shifting_model,price,"");
+            car2 = new CarItem(UUID.randomUUID().toString(), nameCar, horse_power, owners, phone, color ,
+                    car_num, manufacturer, year, Car_model,
+                    test ,kilometre,Engine_capacity,Gear_shifting_model,price,"");
+        }
+        else {
+            car = new Car(nameCar, horse_power, owners, phone, color,
+                    car_num, manufacturer, year, Car_model,
+                    test, kilometre, Engine_capacity, Gear_shifting_model, price, fbs.getSelectedImageURL().toString());
+            car2 = new CarItem(UUID.randomUUID().toString(), nameCar, horse_power, owners, phone, color,
+                    car_num, manufacturer, year, Car_model,
+                    test, kilometre, Engine_capacity, Gear_shifting_model, price, fbs.getSelectedImageURL().toString());
+
+        }
+
+        fbs.getFire().collection("cars").add(car)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getActivity(), "ADD Car is Succesed ", Toast.LENGTH_SHORT).show();
+                        Log.e("addToFirestore() - add to collection: ", "Successful!");
+                        gotoCarList();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("addToFirestore() - add to collection: ", e.getMessage());
+                    }
+                });
+
+        try {
+            fbs.getFire().collection("cars2").add(car2)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            //Toast.makeText(getActivity(), "ADD Car is Succesed ", Toast.LENGTH_SHORT).show();
+                            Log.e("addToFirestore() - add to collection: ", "Successful!");
+                            //gotoCarList();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.e("addToFirestore() - add to collection: ", e.getMessage());
+                        }
+                    });
+        }
+        catch (Exception ex)
+        {
+            Log.e("AddCarFragment: addToFirestore()", ex.getMessage());
+        }
+    }
+
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == getActivity().RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+            img.setImageURI(selectedImageUri);
+            utils.uploadImage(getActivity(), selectedImageUri);
+        }
+    }
+
+    public void gotoCarList() {
+
+        FragmentTransaction ft= getActivity().getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLayout,new CarListMapFragment());
+        ft.commit();
+    }
+
+    public void toBigImg(View view) {
+    }
+
+    /*
+    public void uploadImage(Uri selectedImageUri) {
+        if (selectedImageUri != null) {
+            imageStr = "images/" + UUID.randomUUID() + ".jpg"; //+ selectedImageUri.getLastPathSegment();
+            StorageReference imageRef = fbs.getStorage().getReference().child("images/" + selectedImageUri.getLastPathSegment());
+
+            UploadTask uploadTask = imageRef.putFile(selectedImageUri);
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            //selectedImageUri = uri;
+                            fbs.setSelectedImageURL(uri);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+                    Toast.makeText(getActivity(), "Image uploaded successfully", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getActivity(), "Failed to upload image", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(getActivity(), "Please choose an image first", Toast.LENGTH_SHORT).show();
+        }
+    } */
+}
 
 }
